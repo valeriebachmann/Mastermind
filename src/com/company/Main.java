f6d3776b7c9d2d0704e99449dea2f67f6f517bf2
Natascha Müller <natascha@Nataschas-MacBook-Pro.local>
2020-08-06 12:46:09 +0200

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // vier Farben generieren
+
+
+        
+
+        //Benutzerdefinierte Eingabe
     }
 }
