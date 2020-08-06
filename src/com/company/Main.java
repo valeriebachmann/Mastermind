b5c9398a54dc266a9595c4df70044a10788fc880
Natascha Müller <natascha@Nataschas-MacBook-Pro.local>
2020-08-06 13:43:26 +0200

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,5 +12,24 @@ public class Main {
         
 
         //Benutzerdefinierte Eingabe
+
+
+        Scanner sc = new Scanner(System.in);
+        int[] colors = new int[3];
+
+        System.out.println("Gib eine Farben Konbination ein...");
+        String eingabe = sc.nextLine();
+
+
+        for(int i = 0; i < colors.length; i++){
+            /*if (eingabe.equals(blau||rot||gelb||grün||schwarz||weiss||lila||orange)){
+                System.out.println("Nächste Farbe:");
+                String eingabe2 = sc.nextLine();
+            } else {
+                System.out.println("Diese Farbe gibt es nicht.");
+            }*/
+        }
+
+
     }
 }
